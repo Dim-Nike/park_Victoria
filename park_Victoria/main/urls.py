5cb0ef2fccbd3a8eeb17d2ba65045a50edcdbb4e
Huawey Trigger <trigger@trigger.com>
2023-03-11 17:06:34 +0300

    set contact

--- park_Victoria/main/urls.py
+++ park_Victoria/main/urls.py
@@ -8,6 +8,6 @@ from django.urls import path
 urlpatterns = [
     path('', show_index, name='index'),
     path('products/<int:pk>', show_products, name='products'),
-    path('contact', show_contact, name='contact')
+    path('/contact', show_contact, name='contact')
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
